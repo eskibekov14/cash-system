bbd5079bb76be003ac5b9ffbf3b533d38a0e1c25
Mirat Yeskebekov <eskibekov14@gmail.com>
2025-04-20 13:53:41 +0500

    feature: CSHS-16, added entity SubCategory. (#13)

--- menu-service/src/main/java/kz/cashsystem/menu_service/entity/SubCategory.java
+++ menu-service/src/main/java/kz/cashsystem/menu_service/entity/SubCategory.java
@@ -3,23 +3,22 @@ package kz.cashsystem.menu_service.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "categories")
+@Table(name = "subCategories")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Category {
+public class SubCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "Название категории не должно быть пустым")
     @Size(max = 100, message = "Название категории не должно превышать 100 символов")
     private String name;
-    @ManyToMany(fetch = FetchType.LAZY)
-    private List<SubCategory> subCategories;
 }
