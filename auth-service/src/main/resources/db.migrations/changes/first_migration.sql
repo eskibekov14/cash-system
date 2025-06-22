1dfae706c6f2a57e07965eadc15365b9bc3eebd1
Sultanbek123 <78907422+Sultanbek123@users.noreply.github.com>
2025-06-22 16:48:29 +0500

    CHSH-36 (#31)

---
+++ auth-service/src/main/resources/db.migrations/changes/first_migration.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (
+    username,
+    password,
+    email,
+    created_at,
+    updated_at
+) VALUES (
+             'Sultanbek Kenzhakhimov',
+             '$2a$12$DfqYImPNIqIIS5fG0dP9p.eN.9/bH3PIFHNbVlMwAKCSERR63rdKK',
+             'skenzhahimov@gmail.com',
+             NOW(),
+             NOW()
+);
